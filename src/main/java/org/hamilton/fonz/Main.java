d3578897c67111cce9bf3b6229f0fd27ab7ee9b5
Fonz <Fonz.Hamilton@gmail.com>
2024-09-28 17:14:23 -0700

    Redid project Initialization

---
+++ src/main/java/org/hamilton/fonz/Main.java
@@ -0,0 +1,12 @@
+package org.hamilton.fonz;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
