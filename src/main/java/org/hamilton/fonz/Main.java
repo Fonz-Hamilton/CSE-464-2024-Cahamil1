1aa1454d8e3bbc9c65fffee4a5f31f3d398453dc
Fonz <Fonz.Hamilton@gmail.com>
2024-10-16 23:05:46 -0700

    Added readme pdf

--- src/main/java/org/hamilton/fonz/Main.java
+++ src/main/java/org/hamilton/fonz/Main.java
@@ -6,16 +6,27 @@ public class Main {
         DOTGraph dotGraph = new DOTGraph();
 
         dotGraph.parseGraph("input.dot");
+        System.out.println(dotGraph.toString());
 
         String[] nodesToAdd = {"Z", "X", "Y"};
         dotGraph.addNodes(nodesToAdd);
         System.out.println(dotGraph.toString());
 
-        dotGraph.addEdge("X","Y");
+        String addedNode = "G";
+        dotGraph.addNode(addedNode);
         System.out.println(dotGraph.toString());
 
+        dotGraph.addEdge("X","Y");
+        System.out.println(dotGraph.toString());
+        dotGraph.addEdge("X","Z");
+        System.out.println(dotGraph.toString());
+        dotGraph.addEdge("X","G");
+        System.out.println(dotGraph.toString());
+/*
         dotGraph.outputDOTGraph("output.dot");
 
         dotGraph.outputGraphics("output","png");
+
+         */
     }
 }
