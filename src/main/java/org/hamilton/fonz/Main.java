f5028e7b18fefeb423fa63f09da182ae1cd4e789
Fonz <Fonz.Hamilton@gmail.com>
2024-11-11 00:45:19 -0700

    added updated readme pdf

--- src/main/java/org/hamilton/fonz/Main.java
+++ src/main/java/org/hamilton/fonz/Main.java
@@ -45,11 +45,11 @@ public class Main {
         //dotGraph.removeEdge("a","h");
         //System.out.println(dotGraph.toString());
 
-        Path path = dotGraph.graphSearch(dotGraph.getNode("b"), dotGraph.getNode("h"), DOTGraph.Algorithm.DFS);
+        Path path = dotGraph.graphSearch(dotGraph.getNode("c"), dotGraph.getNode("h"), DOTGraph.Algorithm.DFS);
         System.out.println("DFS: \n" + path.printPath());
 
 
-        path = dotGraph.graphSearch(dotGraph.getNode("b"), dotGraph.getNode("h"), DOTGraph.Algorithm.BFS);
+        path = dotGraph.graphSearch(dotGraph.getNode("a"), dotGraph.getNode("h"), DOTGraph.Algorithm.BFS);
         System.out.println("BFS: \n" + path.printPath());
     }
 }
